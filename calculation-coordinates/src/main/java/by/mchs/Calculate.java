f411a2bfbdddb86cac408f786cf7926910120a1d
vadim1504 <vadi-96@mail.ri>
2017-10-11 01:58:41 +0300

    method comment

--- calculation-coordinates/src/main/java/by/mchs/Calculate.java
+++ calculation-coordinates/src/main/java/by/mchs/Calculate.java
@@ -9,25 +9,31 @@ public class Calculate {
 
     private static final double side = 10000;
 
-    public static Point getCoordinate(ArrayList<Call> c) {
-        ArrayList<Call> calls = new ArrayList<>(c);
+    /** The method calculates the coordinate points of the user to the distance to the towers
+     * The towers are located in the tops of a square with a size of 10000*10000
+     * The method will return Point(x=NaN,y=NaN) if the input parameters are not valid or such a point with such parameters does not exist
+     * @param calls a set of registered calls on towers from this user, at least 2 calls
+     * @return object the Point containing the coordinate points
+     */
+    public static Point getCoordinate(ArrayList<Call> calls) {
+        ArrayList<Call> callscopy = new ArrayList<>(calls);
         Point coordinate = new Point();
-        double min = calls.get(0).getDistance();
+        double min = callscopy.get(0).getDistance();
         int i = 0;
         int id_min = 0;
         int idTowerMin = 0;
-        while (i < calls.size()) {
-            if (calls.get(i).getDistance() <= min) {
-                min = calls.get(i).getDistance();
+        while (i < callscopy.size()) {
+            if (callscopy.get(i).getDistance() <= min) {
+                min = callscopy.get(i).getDistance();
                 id_min = i;
-                idTowerMin = calls.get(i).getIdTower();
+                idTowerMin = callscopy.get(i).getIdTower();
             }
             i++;
         }
-        calls.remove(id_min);
+        callscopy.remove(id_min);
         double[] medians = new double[2];
-        for (i = 0; i < calls.size(); i++) {
-            medians[i] = Math.sqrt(min * calls.get(i).getDistance() * (min + calls.get(i).getDistance() + side) * (min + calls.get(i).getDistance() - side)) / (min + calls.get(i).getDistance());
+        for (i = 0; i < callscopy.size(); i++) {
+            medians[i] = Math.sqrt(min * callscopy.get(i).getDistance() * (min + callscopy.get(i).getDistance() + side) * (min + callscopy.get(i).getDistance() - side)) / (min + callscopy.get(i).getDistance());
         }
         switch (idTowerMin) {
             case 1:
